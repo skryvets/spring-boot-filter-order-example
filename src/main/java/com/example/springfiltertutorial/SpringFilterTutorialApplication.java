bd7719e213f6c0d19c05e25309f5ca44e561fd24
Sergey Kryvets <s.kryvets@gmail.com>
2023-01-08 12:10:29 -0600

    Initial commit

---
+++ src/main/java/com/example/springfiltertutorial/SpringFilterTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfiltertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilterTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilterTutorialApplication.class, args);
+	}
+
+}
